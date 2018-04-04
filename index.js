5119d59bd1ef64bb82398f56a8d605df71bfe397
markdgold <mgoldstein@utexas.edu>
2018-04-04 11:23:27 -0700

    updated config for heroku dbmigrate

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var flash = require('connect-flash');
 var passport = require('./config/passportConfig');
 var db = require('./models');
 var app = express();
+// var pg = require('pg');
 
 //Set and Use
 app.set('view engine', 'ejs');
@@ -27,6 +28,18 @@ app.use(function(req, res, next) {
     next();
 });
 app.use(express.static(__dirname + '/public/'));
+
+// app.get('/db', function (request, response) {
+//   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+//     client.query('SELECT * FROM test_table', function(err, result) {
+//       done();
+//       if (err)
+//        { console.error(err); response.send("Error " + err); }
+//       else
+//        { response.render('pages/db', {results: result.rows} ); }
+//     });
+//   });
+// });
 
 app.get('/', function(req, res) {
     db.climb.findAll({
